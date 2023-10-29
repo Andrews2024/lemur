d8bfeba01d99f86c3e3ceb7d068b2cefd0929633
Ashe <molly.02.andrews@gmail.com>
2023-10-28 21:12:54 -0400

    Added OpenCV smile detection

--- lab0.py
+++ lab0.py
@@ -1,8 +1,22 @@
 # Code modified from https://stackoverflow.com/questions/604749/how-do-i-access-my-webcam-in-python
 import cv2 as cv
 
+# Set up webcam window
 cv.namedWindow('preview')
 vc = cv.VideoCapture(0)
+
+# Set up Haar Cascade Classifier for smiles
+# Modified from https://www.datacamp.com/tutorial/face-detection-python-opencv
+smile_classifier = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_smile.xml")
+
+def detect_bounding_box(vid):
+    gray_image = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)
+    faces = smile_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(50, 100))
+    
+    for (x, y, w, h) in faces:
+        cv.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    
+    return faces
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -10,11 +24,13 @@ else:
     rval = False
 
 while rval:
-    cv.imshow("preview", frame)
     rval, frame = vc.read()
-    key = cv.waitKey(20)
-    if key == 27: # exit on ESC
+    
+    smiles = detect_bounding_box(frame) # Detect smiles and box them
+    cv.imshow("Smile Detection", frame)
+
+    if cv.waitKey(1) == 27: # exit on ESC
         break
 
 vc.release()
-cv.destroyWindow("preview")
+cv.destroyAllWindows()
