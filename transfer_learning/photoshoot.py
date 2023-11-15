ef444ff9864da24e7e039b54d921f37e51db1c0b
Ashe <molly.02.andrews@gmail.com>
2023-11-14 21:34:37 -0500

    Added taking photos and saving to correct folder

--- transfer_learning/photoshoot.py
+++ transfer_learning/photoshoot.py
@@ -1,16 +1,31 @@
 import cv2 as cv
 import os
+from time import sleep
 
 # Create 3 directories, one for each object we're training on
-items = ["pasta-sauce", "pasta", "socks"]
+items = ["pasta-sauce"] #, "pasta", "socks"]
 
 for item in items:
     if not os.path.exists(item):
         os.makedirs(item)
+        print(f"Creating directory /{item}")   
+
+	# Set up the camera
+    vc = cv.VideoCapture(0)
+
+    # Check that it's working
+    result, image = vc.read()
+    
+    if result:
+        for i in range(100): # Take 100 images of the item
+            result, image = vc.read() # Capture image
+            cv.imshow("Photoshoot", image)
+            cv.imwrite(f'{item}/{item}_sample_{i}.png', image) # Save image to file
+
+            sleep(2) # Time for moving object/ changing its pose                
 
 # # Set up the camera
 # cv.namedWindow('preview')
-# vc = cv.VideoCapture(0)
 
 # if vc.isOpened(): # try to get the first frame
 #     rval, frame = vc.read()
@@ -29,4 +44,4 @@ for item in items:
 	# Delay between each picture to change angle
 
 # We're done, close everything down
-# vc.release()
+vc.release()
