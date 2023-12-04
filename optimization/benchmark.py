e6384ea6a0701c13cc1344f26483ccc4d53a9eab
Ashe <molly.02.andrews@gmail.com>
2023-12-03 19:05:46 -0500

    Added load time benchmark

--- optimization/benchmark.py
+++ optimization/benchmark.py
@@ -1,17 +1,43 @@
 from os import stat
+from time import time
+from random import shuffle
+import torch
+
+def load_time(filename: str) -> float:
+    '''Get wall time to load the provided model'''
+    file_type = filename.split('.')[1]
+    device = torch.device("cpu")
+
+    if file_type == "ptl" or "quantized" in filename:
+        start = time()
+        model = torch.jit.load(filename).to(device)
+        end = time()
+        
+        model.eval()
+    
+    else:
+        start = time()
+        model = torch.load(filename).to(device)
+        end = time()
+        
+        model.eval()
+
+    return end - start
 
 if __name__ == '__main__':
-    # Get size of all the files in MB
-    full_original = round(stat('full_model_original.pth').st_size / 1e6, 2)
-    mobile_original = round(stat('mobile_model_original.ptl').st_size / 1e6, 2)
-    full_quantized = round(stat('full_model_quantized.pth').st_size / 1e6, 2)
-    mobile_quantized = round(stat('mobile_model_quantized.ptl').st_size / 1e6, 2)
+    # All current models
+    model_files = ['full_model_original.pth', 'mobile_model_original.ptl', 'full_model_quantized.pth',
+                   'mobile_model_quantized.ptl', 'full_model_pruned.pth', 'mobile_model_pruned.ptl']
+    shuffle(model_files) # Shuffle the list so when we run multiple tests we don't have the same starting file
 
-    print(f"Full original: {str(full_original)} MB\nMobile original: {str(mobile_original)} MB")
-    print(f"Full quantized: {str(full_quantized)} MB\nMobile quantized: {str(mobile_quantized)} MB")
+    # Get time to load each model
+    for model in model_files:
+        print(model)
+        file_size = round(stat(model).st_size / 1e6, 2)
+        model_load_time = load_time(model)
 
-    # et time to load each model
-
+        print(f"Model: {model.split('.')[0]}, Size: {file_size} MB, Load time: {model_load_time}\n")
+    
     # Get time to get a prediction for each model
     
     # Get accuracy of each model on a test set of data
